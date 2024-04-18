447384a0dd9ae0bb5dd386fddb5164a2899dab36
billzayy <coderbillzay@gmail.com>
2024-04-18 14:19:39 +0700

    refactor: path login in /admin/api

--- src/main/java/com/capstone/AdminAuthen/Controller/AuthController.java
+++ src/main/java/com/capstone/AdminAuthen/Controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/admin/api/auth")
 public class AuthController {
 
     @Autowired
